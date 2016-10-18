c516be561894b331e65c43ce876b12785e74aee0
Vlad Mustafin <v.mustafin@gmail.com>
2016-10-18 15:17:04 +0300

    exit page

--- views/site/freereport.php
+++ views/site/freereport.php
@@ -4,12 +4,19 @@ use yii\helpers\Url;
 use yii\bootstrap\BootstrapAsset;
 use yii\web\JqueryAsset;
 
-$this->registerCssFile(Yii::$app->request->baseUrl.'/web/css/fe1.css');
+$this->registerCssFile(Yii::$app->request->baseUrl.'/web/css/fe1.css', ['depends' => [BootstrapAsset::className()]]);
 $this->registerCssFile(Yii::$app->request->baseUrl.'/web/css/exit.css', ['depends' => [BootstrapAsset::className()]]);
 $this->registerJsFile(Yii::$app->request->baseUrl.'/web/js/jquery.animateNumber.min.js', ['depends' => [JqueryAsset::className()]]);
 
+$script_init = <<< JS
+//    var exitsplashmessage = false;
+//    var exitsplashpage = false;
+JS;
+$this->registerJs($script_init, yii\web\View::POS_BEGIN);
+
 $script = <<< JS
-    $('body').addClass('fe2');
+(function ($) {
+    $('body').addClass('fe2 freereport');
     function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
         setInterval(function () {
@@ -19,18 +26,13 @@ $script = <<< JS
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
     
-            display.text("00h : " + minutes + "m: " + seconds + "s");
+            display.text("00h : " + minutes + "m : " + seconds + "s");
     
             if (--timer < 0) {
                 timer = duration;
             }
         }, 1000);
     }
-    jQuery(function ($) {
-        var fiveMinutes = 60 * 5,
-            display = $('#time-exit');
-        startTimer(fiveMinutes, display);
-    });
 
    function changeFSize()
     {
@@ -130,37 +132,47 @@ $script = <<< JS
         return Math.random() * (max - min) + min;
     }
 
-    function peopleFilling()
-    {
-        //doSomething();
-        (function loop() {
-            var rand =  getRandomArbitrary(4000,9000);
-            setTimeout(function() {doSomething(); loop();}, rand);
-        }());
-    }
-    peopleFilling();
+    (function loop() {
+        var rand =  getRandomArbitrary(4000,9000);
+        setTimeout(function() {
+            doSomething();
+            loop();
+        }, rand);
+    }());
+
+    // onload actions
+    $(function ($) {
+        var fiveMinutes = 60 * 5,
+            display = $('#time-exit');
+        startTimer(fiveMinutes, display);
+    });
+    
+    $('input[type=text], input[type=email]').on('change', function(){
+        if ($(this).val()) {
+           $(this).addClass('filled');
+        } else {
+           $(this).removeClass('filled');
+        }
+    });
+})(jQuery);
 JS;
 $this->registerJs($script, yii\web\View::POS_READY);
 ?>
-<div class="exit-background">
+
+<div class="image-background">
+    <div class="bg">
         <div class="container header">
             <div class="row">
-                <div class="col-xs-6 logo vcenter col-lg-2">
+                <div class="col-xs-12 logo vcenter col-lg-2">
                     <img src="images/ClickMoneyLogo/Logo-white.svg">
-                </div>
-                <div class="col-xs-6 lic text-right vcenter col-lg-1 pull-right">
+                </div><!--
+                --><div class="col-xs-12 col-lg-8 vcenter text-white">
+                    <img src="images/nn.png">There are currently
+                    <span class="feExitRed" id="people_filling_nd_spot">416</span> People On This Very Page Right Now.
+                    <span class="feExitRed" id="try_to_take_spot">104</span> of Them Trying To Take <span class="feExitBold">Your Report</span> Right Now. <span class="feExitBold">Act Quickly!</span>
+                </div><!--
+                --><div class="col-xs-12 lic text-right vcenter col-lg-2">
                     <span class="licenceLeft">1</span><span class="licenceRight">Report Left</span>
-                </div>
-                <div class="col-xs-12 col-lg-9 text-white row">
-                    <div class="col-lg-2 col-md-12">
-                        <img src="images/nn.png">There are currently
-                    </div>
-                    <div class="col-lg-4 col-md-12">
-                        <span class="feExitRed" id="people_filling_nd_spot">416</span> People On This Very Page Right Now.
-                    </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span class="feExitRed" id="try_to_take_spot">104</span> of Them Trying To Take <span class="feExitBold">Your Report</span> Right Now. <span class="feExitBold">Act Quickly!</span>
-                    </div>
                 </div>
 
             </div>
@@ -176,15 +188,14 @@ $this->registerJs($script, yii\web\View::POS_READY);
                 --><div class="col-md-5 col-xs-12 vtop right_block">
                     <div class="row text-center">
                         <div class="col-xs-12">
-                            <h2 class="clickbtFE2 feExitWar0"><span class="feExitWar">WARNING:</span> Download Your Free ClickMoney Report Before This Exclusive Offer Expires!</h2>
+                            <h2 class="exitWarning"><span>WARNING:</span> Download Your<br/><strong>Free ClickMoney Report</strong> Before<br/>This Exclusive Offer Expires!</h2>
                         </div>
                         <div class="col-xs-12">
-                            <p class="notifExit">Enter your first name and best email address below to proceed:</p>
+                            <p class="exitNotification">Enter your first name and best email address below to proceed:</p>
                         </div>
                     </div><!--
                     --><div class="row action-form">
                         <div class="col-xs-12">
-                            <img class="main-arrow" src="images/arfe3.png">
                             <form method="post" action="">
                                 <div class="form-fields">
                                     <div class="row">
@@ -206,6 +217,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                                 </div>
                                 <div class="row">
                                     <div class="col-xs-12 text-center">
+                                        <img class="main-arrow hidden-md hidden-sm hidden-xs" src="images/arfe3.png">
                                         <button type="submit">
                                             <div class="row">
                                                 <div class="col-md-12">
@@ -248,138 +260,5 @@ $this->registerJs($script, yii\web\View::POS_READY);
                 </div>
             </div>
         </div>
+    </div>
 </div>
-
-
-<script src="js/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script src="https://s3.amazonaws.com/caff/js/formhelpers.min.js"></script>
-<script type="text/javascript">
-    (function($) {
-//        setInterval(function(){
-//            $(".join-popup").addClass('showed');
-//            setTimeout(function(){
-//                $(".join-popup").removeClass('showed');
-//            }, 2000);
-//        }, 5000);
-//        $(".join-popup .close-btn a").click(function(e){
-//            $(".join-popup").removeClass('showed');
-//            return false;
-//        });
-    })(jQuery);
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--
-
-
-        <div class="clickthrough">
-            <div class="fe-exit">
-                <div class="container header">
-                    <div class="row">
-                        <div class="col-md-2 col-xs-12 logo1 col-sm-3">
-                            <img src="/images/register/logoWhite.png">
-                        </div>
-                        <div class="col-md-8 col-xs-12 fex-text col-sm-6">
-                            <img src="/images/nn.png">
-                            There are currently <span class="feExitRed">416</span> People On This Very Page Right Now. <span class="feExitRed">104</span> of Them Trying To Take <span class="feExitBold">Your Report</span> Right Now. <span class="feExitBold">Act Quickly!</span>
-                        </div>
-                        <div class="col-md-2 col-xs-12 lic col-sm-3">
-                            <div class="row">
-                                <div class="col-md-8 licenceRight col-sm-7">
-                                    <span>Report Left</span>
-                                </div>
-                                <div class="col-md-4 licenceLeft1 col-sm-5">
-                                    <span>1</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container content ">
-                    <div class="row content0">
-                        <div class="col-md-7 left_block">
-                            <div class="embed-responsive-item video_through video-img">
-                                <img class="play-button" src="images/play.png">
-                            </div>
-                        </div>
-                        <div class="col-md-5 right_block conFE2">
-                            <h2 class="clickbtFE2 feExitWar0"><span class="feExitWar">WARNING:</span> Download Your <span class="bold-text">Free ClickMoney Report</span> Before This Exclusive Offer Expires!</h2>
-                            <p class="notifExit" >Enter your first name and best email address below to proceed:</p>
-                            <div class="col-md-10 initial col-xs-10">
-                                <div>
-                                    <div class="row">
-                                        <div class="col-md-5 nameFE20 col-xs-12">My name's</div>
-                                        <input id="modal-name" class="col-md-7 nameFE2 nameExit col-xs-12" placeholder="John">
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-5 emailFE20 col-xs-12">My best email is</div>
-                                        <input id="modal-email" class="col-md-7 emailFE2 col-xs-12" placeholder="john@example.com">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-2 col-xs-2">
-                                <img class="arfe3" src="/images/arfe3.png">
-                            </div>
-<!--                            <a href="">-->
-                <!--            <button id="modal-submit" class="col-sm-8 button1 buttonEXRed col-md-9 col-md-offset-1 col-xs-7 col-xs-offset-0 col-lg-10 col-lg-offset-1">
-                                <div class="col-md-12 col-xs-12 proceed btnFE2 btnFE3">
-                                    <span>FAST! CLICK HERE TO GET THE REPORT</span>
-                                </div>
-                            </button>
-<!--                            </a>-->
-              <!--              <div class="row">
-                                <div class="col-md-12">
-                                    <div class="col-md-11 col-lg-10 col-lg-offset-1 blockText blockText1 col-xs-12">
-                                        <img class="blockImg" src="/images/block.png">
-                                        The Last Spot Is Still Here. Grab it Before Someone Else Does!
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-10 col-xs-10 col-xs-offset-1 exitDown">
-                                <span>HURRY!</span> Download Link Expires in: <span>00h : 03m : 27s</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container footer">
-                    <div class="row">
-                        <div class="col-md-2 col-xs-12 logWhite col-lg-2 col-sm-2"><a href=""><img src="/images/register/logoWhiteL.png"></a></div>
-                        <div class="col-md-7 col-xs-12 col-lg-7 col-sm-7 copyright1">
-                            <ul class="fnavigation">
-                                <li><a href="<!?= Url::toRoute(['site/disclaimer']); ?>" target="_blank">Government Disclaimer | </a></li>
-                                <li><a href="<!?= Url::toRoute(['site/privacy-policy']); ?>" target="_blank">Privacy Policy | </a></li>
-                                <li><a href="<!?= Url::toRoute(['site/terms']); ?>" target="_blank">Terms | </a></li>
-                                <li><a href="<!?= Url::toRoute(['site/earnings-disclaimer']); ?>" target="_blank">Earnings Disclaimer | </a></li>
-                                <li><a href="<!?= Url::toRoute(['site/spam-policy']); ?>" target="_blank">Spam Policy | </a></li>
-                                <li><a href="mailto: <!?= Yii::$app->params['support_email']; ?>">Support</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3 copyright1 col-lg-3 col-sm-3">
-                            <span class="copyright">© <!?= date('Y'); ?> ClickMoney. All Rights Reserved.</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
--->
