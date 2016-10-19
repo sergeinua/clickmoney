c49a7e9fb474404743df47424074cae78b6a8784
sergei <sergei.nikonov@yahoo.com>
2016-10-19 12:20:20 +0300

    settings for the aweber form updated

--- config/params.php
+++ config/params.php
@@ -22,6 +22,7 @@ return [
 //            'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
 //            'redirect_id' => 'redirect_9a6c3ca8343e08a00a4e50f8f6c914bf',
 //            'meta_adtracking' => 'My_New_Web_Form',
+//            'meta_message' => '1',
 //            'img_url' => 'https://forms.aweber.com/form/displays.htm?id=7CwM7IzsDEyc'
 //        ]
 
@@ -68,6 +69,7 @@ return [
 //            'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
 //            'redirect_id' => 'redirect_d2d2ef02abb1c3958c04f8f3f1030cbe',
 //            'meta_adtracking' => 'My_Web_Form',
+//            'meta_message' => '1',
 //            'img_url' => 'https://forms.aweber.com/form/displays.htm?id=HCxMDExM7IwM'
 //        ]
     ]
--- views/site/forms/form_aweber.php
+++ views/site/forms/form_aweber.php
@@ -1,25 +1,19 @@
-<!--
-
-<form method="post" id="squeeze_form1" class="af-form-wrapper" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl"  >
+<form method="post" id="squeeze_form<?= $item; ?>" class="af-form-wrapper" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl" style="display: none;" >
     <div style="display: none;">
-        <input type="hidden" name="meta_web_form_id" value="842022710" />
+        <input type="hidden" name="meta_web_form_id" value="<?= $params['meta_web_form_id']; ?>" />
         <input type="hidden" name="meta_split_id" value="" />
-        <input type="hidden" name="listname" value="awlist4444369" />
-        <input type="hidden" name="redirect" value="https://www.aweber.com/thankyou.htm?m=default" id="redirect_d2d2ef02abb1c3958c04f8f3f1030cbe" />
+        <input type="hidden" name="listname" value="<?= $params['listname']; ?>" />
+        <input type="hidden" name="redirect" value="<?= $params['redirect_url']; ?>" id="<?= $params['redirect_id']; ?>" />
 
-        <input type="hidden" name="meta_adtracking" value="My_Web_Form" />
-        <input type="hidden" name="meta_message" value="1001" />
+        <input type="hidden" name="meta_adtracking" value="<?= $params['meta_adtracking']; ?>" />
+        <input type="hidden" name="meta_message" value="<?= $params['meta_message']; ?>" />
         <input type="hidden" name="meta_required" value="name,email" />
 
         <input type="hidden" name="meta_tooltip" value="" />
     </div>
-    <input name="name" id="squeeze_form1_name" value="cfoverlayfriend" type="text">
-    <input class="text" name="email" id="squeeze_form1_email" value="" type="text">
-
-
-    <div style="display: none;"><img src="https://forms.aweber.com/form/displays.htm?id=HCxMDExM7IwM" alt="" /></div>
+    <input name="name" id="squeeze_form<?= $item; ?>_name" value="cfoverlayfriend" type="text">
+    <input class="text" name="email" id="squeeze_form<?= $item; ?>_email" value="" type="text">
+    <div style="display: none;"><img src="<?= $params['img_url']; ?>" alt="" /></div>
 </form>
+<iframe name="squeeze_iframe<?= $item; ?>" style="display:none"></iframe>
 
-<iframe name="squeeze_iframe1" style="display:none"></iframe>
--->
-<!-- AWeber Web Form Generator 3.0.1 -->
