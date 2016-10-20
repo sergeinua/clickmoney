63c17ac2a5269f6ce3f6b9b07837a3da3d7d7afd
sergei <sergei.nikonov@yahoo.com>
2016-10-20 17:36:54 +0300

    from_page now including mobile version

--- web/js/main.js
+++ web/js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $('#main-form').on('submit', function(){
         var email_value = $('#email').val();
         if (typeof from_page == 'undefined') {
-            from_page = 'fp';
+            from_page = 'fe';
+        }
+        if (from_page == 'mobile') {
+            from_page = 'fe_mobile';
         }
         var fname = $('#name').val();
         validateEmail(email_value, from_page, fname);
@@ -11,16 +14,26 @@ $(document).ready(function(){
 
     $('#freereport-form').on('submit', function () {
         var email = $('#freereport-form #email').val(),
-            name = $('#freereport-form #name').val(),
+            name = $('#freereport-form #name').val();
+        if (typeof from_page == 'undefined') {
             from_page = 'exit';
+        }
+        if (from_page == 'mobile') {
+            from_page = 'exit_mobile';
+        }
         validateEmail(email, from_page, name);
         window.onbeforeunload = null;
     });
 
     $('#exit-popup-form').on('submit', function () {
         var name = $('#exitpopup #name').val(),
-            email = $('#exitpopup #email').val(),
-            from_page = "overlay";
+            email = $('#exitpopup #email').val();
+        if (typeof from_page == 'undefined') {
+            from_page = 'overlay';
+        }
+        if (from_page == 'mobile') {
+            from_page = 'overlay_mobile';
+        }
         validateEmail(email, from_page, name);
         window.onbeforeunload = null;
     });
