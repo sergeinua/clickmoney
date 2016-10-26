67ce828d130b7a42bf871f31b128884ab7a5d0bb
sergei <sergei.nikonov@yahoo.com>
2016-10-26 18:37:15 +0300

    migration: table name fixed

--- migrations/m161003_071859_create_table_cm_all_adds.php
+++ migrations/m161003_071859_create_table_cm_all_adds.php
@@ -2,11 +2,11 @@
 
 use yii\db\Migration;
 
-class m161003_071859_create_table_sf_all_adds extends Migration
+class m161003_071859_create_table_cm_all_adds extends Migration
 {
     public function up()
     {
-        $this->createTable('sf_all_adds', [
+        $this->createTable('cm_all_adds', [
             'id' => $this->primaryKey(),
             'info_user' => $this->string(255)->notNull(),
             'page_type' => $this->string(10)->notNull(),
@@ -17,19 +17,8 @@ class m161003_071859_create_table_sf_all_adds extends Migration
 
     public function down()
     {
-        echo "m161003_071859_create_table_sf_all_adds cannot be reverted.\n";
+        echo "m161003_071859_create_table_cm_all_adds cannot be reverted.\n";
 
         return false;
     }
-
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
-
-    public function safeDown()
-    {
-    }
-    */
 }
