ae8511169904583dfe726aa0f1980ee88412c8a2
sergei <sergei.nikonov@yahoo.com>
2016-10-27 14:39:50 +0300

    approved page: link for scrolling bottom form updated

--- views/site/approved.php
+++ views/site/approved.php
@@ -107,7 +107,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                 </div>
             </div>
         </div>
-        <div class="container left-right-container" id="top-form">
+        <div class="container left-right-container">
             <div class="row">
                 <div class="col-md-7 left_block">
                     <img class="img-responsive img-membership1" src="images/video(16-9).jpg">
@@ -1073,7 +1073,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                         <p class="blank-text">Join Our Successful</p>
                         <p class="blank-text">Members Club!</p>
                         <div class="register-btn">
-                            <a href="#top-form"><p>REGISTER YOUR CLICKMONEY</p></a>
+                            <a href="#bottom-form"><p>REGISTER YOUR CLICKMONEY</p></a>
                         </div>
                     </div>
                 </div>
@@ -1113,7 +1113,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                         <p class="blank-text">Join Our Successful</p>
                         <p class="blank-text">Members Club!</p>
                         <div class="register-btn">
-                            <a href="#top-form"><p>REGISTER YOUR CLICKMONEY</p></a>
+                            <a href="#bottom-form"><p>REGISTER YOUR CLICKMONEY</p></a>
                         </div>
                     </div>
                 </div>
@@ -1153,7 +1153,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                         <p class="blank-text">Join Our Successful</p>
                         <p class="blank-text">Members Club!</p>
                         <div class="register-btn">
-                            <a href="#top-form"><p>REGISTER YOUR CLICKMONEY</p></a>
+                            <a href="#bottom-form"><p>REGISTER YOUR CLICKMONEY</p></a>
                         </div>
                     </div>
                 </div>
@@ -1287,7 +1287,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                -->
     </div>
 </section>
-<section class="last-chace-register">
+<section class="last-chace-register" id="bottom-form">
     <div class="container">
         <div class="row qick-and-easy">
             <div class="text-center">
