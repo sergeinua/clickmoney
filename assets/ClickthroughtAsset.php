1fab063a83bf1da9e87f67af695b8d4fbb592a9c
Vlad Mustafin <v.mustafin@gmail.com>
2016-10-28 20:45:06 +0300

    clickthrought page

---
+++ assets/ClickthroughtAsset.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class ClickthroughtAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/fe1.css',
+        'css/style_click_through.css'
+    ];
+    public $js;
+    public $depends = [
+        'app\assets\AppAsset'
+    ];
+
+    public function init()
+    {
+        $this->js = [
+            'exitsplash',
+            'https://player.vimeo.com/api/player.js'
+        ];
+
+        parent::init();
+    }
+}
--- views/site/accessapproved.php
+++ views/site/accessapproved.php
@@ -2,10 +2,9 @@
 use  Mobile_Detect;
 
 use yii\helpers\Url;
-use app\assets\AppAsset;
+use app\assets\ClickthroughtAsset;
 
-$this->registerCssFile(Yii::$app->request->baseUrl.'/web/css/style_click_through.css', ['depends' => [AppAsset::className()]]);
-$this->registerJsFile('/exitsplash', ['depends' => [AppAsset::className()]]);
+ClickthroughtAsset::register($this);
 
 Yii::$app->params['bodyClass'] = 'clickthrough';
 
@@ -23,42 +22,77 @@ if ($mob->isTablet() || $mob->isMobile()) {
 $this->title = 'ClickMoney.com';
 
 ?>
+
 <div class="container header">
     <div class="row">
-        <div class="col-md-7 col-xs-4 logo1">
-            <img class="img-responsive" src="/images/ClickMoneyLogo/Logo-white.svg">
+        <div class="col-xs-6 logo vcenter">
+            <img src="images/ClickMoneyLogo/Logo-white.svg">
         </div>
-        <div class="col-md-5 col-md-offset-7 col-xs-8 lic">
-            <div class="col-md-4 licenceRight"><span>Licence Left</span></div>
-            <div class="col-md-1 licenceLeft"><span>1</span></div>
+
+        <div class="col-xs-6 col-md-offset-2 lic text-right vcenter">
+            <span class="licenceRight">Licence Left</span><span class="licenceLeft">1</span>
         </div>
     </div>
 </div>
 <div class="container content">
-    <div class="row content0">
-        <div class="col-md-7 left_block left_block1">
+    <div class="row">
+        <div class="col-md-7 col-xs-12 video-through vcenter text-center left_block">
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item"
                         src="https://player.vimeo.com/video/189163302?autoplay=true" width="auto"
                         height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
-        </div>
-        <div class="col-md-5 right_block right_block1">
-            <p class="welkGreen">WELCOME ABOARD!</p>
-            <p class="clickbt">Click The Button Below</p>
-            <p class="secure">To <span class="span-secr">Secure </span><span class="orange">1</span><span class="span-secr"> of the</span> <span class="orange">16</span> CLICK MONEY Biz Memberships</p>
-            <div class="stop col-md-9 col-xs-9">
-                <div class="col-md-3 col-xs-4">
-                    <img src="/images/stop.png" class="img-responsive img_stop">
+            <!--            <img class="img-responsive" src="images/video(16-9).jpg">-->
+            <!--            <img class="play" src="images/play.png" />-->
+            <!--<img class="play_gd" src="images/gd_corner_fe1.png" />-->
+            <!--<img class="play_logo" src="images/ClickMoneyLogo/Logo-green.svg" />-->
+        </div><!--
+                --><div class="col-md-5 col-xs-12 vcenter right_block">
+            <div class="row text-uppercase text-center">
+                <div class="col-xs-12">
+                            <span class="main-text">
+                                 <p class="welkGreen">WELCOME ABOARD!</p>
+                                <p><u><strong>Click The Button Below</strong></u><br>
+                                 To <u><strong>Secure </strong></u><span class="orange">1</span><u><strong> of the</strong></u> <span class="orange">10</span>
+                                     CLICK MONEY LICENSES</p>
+                            </span>
                 </div>
-                <div class="do_not_ex0 col-md-8 col-xs-8">
-                    <p class="stop_do_not_ex orange">Please <span class="red">DO NOT</span> Exit This Page!</p>
-                    <p class="stop_do_not_ex1"">Or your spot will be given to the next person in line waiting to watch this video.</p>
+            </div><!--
+                    --><div class="row action-form">
+                <div class="col-xs-12">
+                    <div class="stop container-fluid">
+                        <div class="row">
+                                <div class="col-md-3 col-xs-4">
+                                    <img src="/images/stopRed.png" class="img-responsive img_stop">
+                                </div>
+                                <div class="do_not_ex0 col-md-8 col-xs-8">
+                                    <p class="stop_do_not_ex orange">Please <span class="red">DO NOT</span> Exit This Page!</p>
+                                    <p class="stop_do_not_ex1"">Or your spot will be given to the next person in line waiting to watch this video.</p>
+                                </div>
+                            </div>
+                        <img src="/images/arrow-for-throu.png" class="img-responsive arr1">
+
+                    </div>
+
+
                 </div>
-                <img src="/images/arrow-for-throu.png" class="img-responsive arr1">
             </div>
-            <button type="submit" class="button1 button02" onclick="window.onbeforeunload = null; window.location='/approved';">CLICK HERE TO PROCEED<img class="button-arrow" src="/images/arr.png"></button>
-            <div class="button2 col-md-10 col-xs-12"><span class="button20">REMINDER:</span> <span class="orange"><span class="big-number" style="font-size: 14px;">1</span> LICENSE LEFT</span></div>
+            <div class="row action-form">
+                <div class="col-xs-12 text-center">
+                    <button type="submit" onclick="window.onbeforeunload = null; window.location='/approved';">
+                        <div class="row">
+                            <div class="col-md-11 col-xs-9">
+                                CLICK HERE TO PROCEED
+                            </div>
+                            <div class="arrow">
+                                <img src="images/arrow-fe-1.png" />
+                            </div>
+                        </div>
+                    </button>
+                </div>
+            </div>
+                <div class="button2 col-md-12 col-xs-12"><span class="button20">REMINDER:</span> <span class="orange"><span class="big-number" style="font-size: 14px;">1</span> LICENSE LEFT</span></div>
+
         </div>
     </div>
 </div>
@@ -82,3 +116,4 @@ $this->title = 'ClickMoney.com';
         </div>
     </div>
 </div>
+
