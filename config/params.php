69b67dd9a1d1d66a4c611ceb4ac5fe6bbdc20a24
sergei <sergei.nikonov@yahoo.com>
2016-10-31 12:36:05 +0200

    forms: aweber form redirect_id changed

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ $forms = [
         'meta_web_form_id' => '180194731',
         'listname' => 'awlist4472749',
         'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
-        'redirect_id' => 'redirect_5ebab7c26fbc3a03cf1dcd0bff6229ad',
+        'redirect_id' => 'redirect_03fe817eefa01a03e967977a8c340f50',
         'meta_adtracking' => 'CM_form',
         'meta_message' => '1',
         'img_url' => 'https://forms.aweber.com/form/displays.htm?id=jBwMjJws7MyM'
