29cfa30e082babed4b1bd1b5edec4fbbdd562fe6
sergei <sergei.nikonov@yahoo.com>
2016-10-31 15:53:26 +0200

    table name updated

--- models/Subscriber.php
+++ models/Subscriber.php
@@ -20,7 +20,7 @@ class Subscriber extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return 'sf_all_adds';
+        return 'cm_all_adds';
     }
 
     /**
