27052dfc0a3379c4f37a41ffa3576e8f95f154bf
sergei <sergei.nikonov@yahoo.com>
2016-10-31 11:36:29 +0200

    forms added

--- config/params.php
+++ config/params.php
@@ -1,86 +1,34 @@
 <?php
 
+$forms = [
+    'ymlp' => [
+        'action' => 'https://ymlp.com/subscribe.php?id=gwbqjmugmgb'
+    ],
+    'aweber' => [
+        'meta_web_form_id' => '180194731',
+        'listname' => 'awlist4472749',
+        'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
+        'redirect_id' => 'redirect_5ebab7c26fbc3a03cf1dcd0bff6229ad',
+        'meta_adtracking' => 'CM_form',
+        'meta_message' => '1',
+        'img_url' => 'https://forms.aweber.com/form/displays.htm?id=jBwMjJws7MyM'
+    ],
+];
+
 return [
     'adminEmail' => 'admin@example.com',
     'domain_name' => 'ClickMoneySystem.com',
     'company_name' => 'ClickMoneySystem',
     'support_email' => 'support@clickmoneysystem.com',
-    'esp_forms' => [
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbjuhegmgb'
-//        ],
+    'esp_forms' => $forms,
+    'esp_forms_exit' => $forms,
+    'esp_forms_overlay' => $forms
+//        [
 //        'mailminion' => [
 //            'action' => 'https://dashboard.mailminion.com/lists/vb520bcxxo880/subscribe'
 //        ],
 //        'getresponse' => [
 //            'campaign_token' => 'nciX6',
-//
 //        ],
-//        'aweber' => [
-//            'meta_web_form_id' => '969251005',
-//            'listname' => 'awlist4466731',
-//            'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
-//            'redirect_id' => 'redirect_5ebab7c26fbc3a03cf1dcd0bff6229ad',
-//            'meta_adtracking' => 'FE',
-//            'meta_message' => '1',
-//            'img_url' => 'https://forms.aweber.com/form/displays.htm?id=nGycTKyMDAys'
-//        ],
-//
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbqhhugmgb'
-//        ],
-
-    ],
-    'esp_forms_exit' => [
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbjuhegmgb'
-//        ],
-//        'mailminion' => [
-//            'action' => 'https://dashboard.mailminion.com/lists/vb520bcxxo880/subscribe'
-//        ],
-//        'getresponse' => [
-//            'campaign_token' => 'nciX6',
-//
-//        ],
-//
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbqhhugmgb'
-//        ],
-//
-//        'aweber' => [
-//            'meta_web_form_id' => '842022710',
-//            'listname' => 'awlist4444369',
-//            'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
-//            'redirect_id' => 'redirect_d2d2ef02abb1c3958c04f8f3f1030cbe',
-//            'meta_adtracking' => 'My_Web_Form',
-//            'meta_message' => '1',
-//            'img_url' => 'https://forms.aweber.com/form/displays.htm?id=HCxMDExM7IwM'
-//        ],
-    ],
-    'esp_forms_overlay' => [
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbjuhegmgb'
-//        ],
-//        'mailminion' => [
-//            'action' => 'https://dashboard.mailminion.com/lists/vb520bcxxo880/subscribe'
-//        ],
-//        'getresponse' => [
-//            'campaign_token' => 'nciX6',
-//
-//        ],
-//
-//        'ymlp' => [
-//            'action' => 'https://ymlp.com/subscribe.php?id=gwbqhhugmgb'
-//        ],
-//
-//        'aweber' => [
-//            'meta_web_form_id' => '842022710',
-//            'listname' => 'awlist4444369',
-//            'redirect_url' => 'https://www.aweber.com/thankyou.htm?m=default',
-//            'redirect_id' => 'redirect_d2d2ef02abb1c3958c04f8f3f1030cbe',
-//            'meta_adtracking' => 'My_Web_Form',
-//            'meta_message' => '1',
-//            'img_url' => 'https://forms.aweber.com/form/displays.htm?id=HCxMDExM7IwM'
-//        ]
-    ]
+//    ]
 ];
