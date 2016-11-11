5e093cf70564f99b0edace6acc522c4a9b56f21c
sergei <sergei.nikonov@yahoo.com>
2016-11-11 17:24:40 +0200

    aweber api:  now adding SubscriberApi model for aweber and commented code removed

--- commands/AutoresponderController.php
+++ commands/AutoresponderController.php
@@ -62,81 +62,6 @@ class AutoresponderController extends Controller
     }
 
     /**
-     * Checks if there're any models to be sent
-     */
-    public function Check()
-    {
-//        /* ymlp models to be sent */
-//        $ymlp_models = SubscriberApi::find()
-//            ->where(['api_id' => Apis::find()->where(['name' => 'ymlp'])->one()['id']])
-//            ->andWhere(['status' => 0])
-//            ->all();
-//
-//        /* ymlp api */
-//        $ymlp_api_key = Yii::$app->params['ymlp_api_key'];
-//        $ymlp_api_username = Yii::$app->params['ymlp_api_username'];
-//        $ymlp = new YMLP_API($ymlp_api_key, $ymlp_api_username);
-//
-//        foreach ($ymlp_models as $model) {
-//            $email = Subscriber::find()->where(['id' => $model['subscriber_id']])->one()['info_user'];
-//            $ymlp_email_exists = false;
-//            $ymlp_added = false;
-//            /* checking if email exists */
-//            if (isset($ymlp->ContactsGetContact($email)['EMAIL'])) {
-//                $ymlp_email_exists = true;
-//            } else {
-//                /* creating new subscriber */
-//                $ymlp->ContactsAdd($Email = $email, $OtherFields = '', $GroupID = '1', $OverruleUnsubscribedBounced = '1');
-//                $ymlp_added = true;
-//            }
-//            if ($ymlp_added) {
-//                $api_model = SubscriberApi::find()->where(['id' => $model['id']])->one();
-//                $api_model->added_at = time();
-//                $api_model->status = 1;
-//                $api_model->save();
-//            }
-//        }
-
-//        /* aweber models to be sent */
-//        $aweber_models = SubscriberApi::find()
-//            ->where(['api_id' => Apis::find()->where(['name' => 'aweber'])->one()['id']])
-//            ->andWhere(['status' => 0])
-//            ->limit(25)
-//            ->all();
-//
-//        /* aweber api */
-//        if (count($aweber_models) > 0) {
-//            $consumerKey = Yii::$app->params['aweber_consumer_key'];
-//            $consumerSecret = Yii::$app->params['aweber_consumer_secret'];
-//            $accessToken = Yii::$app->params['aweber_access_token'];
-//            $accessTokenSecret = Yii::$app->params['aweber_access_token_secret'];
-//            $list_id = Yii::$app->params['aweber_list_id'];
-//            $aweber = new MainAweberApi($consumerKey, $consumerSecret);
-//            $account_id = $aweber->getAccount($accessToken, $accessTokenSecret);
-//            /* checking for existing subscriber in the local copy of the aweber subscribers */
-//            foreach ($aweber_models as $model) {
-//                $email = Subscriber::find()->where(['id' => $model['subscriber_id']])->one()['info_user'];
-//                $aweber_email_exists = false;
-//                $aweber_added = false;
-//                /* checking if email exists */
-//                $aweber_email_exists = AweberExistingSubscribers::find()->where(['email' => $email])->exists();
-//                if (! $aweber_email_exists) {
-//                    $subscriber_added = $aweber->addSubscriber($accessToken, $accessTokenSecret, $account_id, $list_id, ['email' => $email]);
-//                    if ($subscriber_added) {
-//                        $aweber_added = true;
-//                    }
-//                }
-//                if ($aweber_added) {
-//                    $api_model = SubscriberApi::find()->where(['id' => $model['id']])->one();
-//                    $api_model->added_at = time();
-//                    $api_model->status = 1;
-//                    $api_model->save();
-//                }
-//            }
-//        }
-    }
-
-    /**
      * Updates table of the aweber's account subscribers
      *
      */
@@ -163,13 +88,22 @@ class AutoresponderController extends Controller
                 AweberExistingSubscribers::deleteAll();
                 $time = time();
                 foreach ($existing_subscribers as $item) {
-                    if (! $this->checkIfMissingDb($item['email']))
-                        (new Subscriber([
-                            'info_user' => $item['email'],
-                            'page_type' => 'AweberApi',
-                            'ip' => is_null($item['ip_address']) ? '127.0.0.1' : $item['ip_address'],
-                            'created_at' => date('Y-m-d H:i:s', $time)
-                        ]))->save(false);
+                    if (! $this->checkIfMissingDb($item['email'])) {
+                        /* adding to db */
+                        $subscriber = new Subscriber();
+                        $subscriber->info_user = $item['email'];
+                        $subscriber->page_type = 'AweberApi';
+                        $subscriber->ip = is_null($item['ip_address']) ? '127.0.0.1' : $item['ip_address'];
+                        $subscriber->created_at = date('Y-m-d H:i:s', $time);
+                        $subscriber->save(false);
+                        /* adding with status 1 - for not taking this record in further requests */
+                        (new SubscriberApi([
+                            'subscriber_id' => $subscriber->id,
+                            'api_id' => Apis::find()->where(['name' => 'aweber'])->one()['id'],
+                            'added_at' => time(),
+                            'status' => 1
+                        ]))->save();
+                    }
                     (new AweberExistingSubscribers([
                         'list_id' => $list_id,
                         'email' => $item['email'],
