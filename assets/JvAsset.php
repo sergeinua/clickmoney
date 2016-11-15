5f43ef0b977de6dd63fa8ae3861ff805b7582a6a
sergei <sergei.nikonov@yahoo.com>
2016-11-15 13:23:01 +0200

    JV page

--- assets/JvAsset.php
+++ assets/JvAsset.php
@@ -14,11 +14,7 @@ class JvAsset extends AppAssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'contest/css/demo.css',
-        'contest/css/powerange.css',
-        'contest/css/leaderboard_new.css?v=127',
-        'contest/css/animate.css',
-        'http://fonts.googleapis.com/css?family=Lato:400,700,900'
+        'css/jv.css'
     ];
     public $js;
     public $depends = [
@@ -28,12 +24,6 @@ class JvAsset extends AppAssetBundle
     public function init()
     {
         $this->js = [
-//            'js/main.js',
-            'contest/js/bootstrap.min.js',
-            'contest/js/contest-2.js?v=4',
-            'contest/js/powerange.js',
-            'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js',
-            'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'
         ];
 
         parent::init();
---
+++ assets/JvMainAsset.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class JvMainAsset extends AppAssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'contest/css/demo.css',
+        'contest/css/powerange.css',
+        'contest/css/leaderboard_new.css?v=127',
+        'contest/css/animate.css',
+        'http://fonts.googleapis.com/css?family=Lato:400,700,900'
+    ];
+    public $js;
+    public $depends = [
+        'app\assets\AppAsset'
+    ];
+
+    public function init()
+    {
+        $this->js = [
+//            'js/main.js',
+            'contest/js/bootstrap.min.js',
+            'contest/js/contest-2.js?v=4',
+            'contest/js/powerange.js',
+            'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js',
+            'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'
+        ];
+
+        parent::init();
+    }
+}
